7aff7b9e4f835534c3f1236c2932e78b3c62a680
Taimaru Provensal <taimaru@gmail.com>
2019-12-14 14:09:00 -0500

    finished ex10 with extra

---
+++ lvl2/ex10.py
@@ -0,0 +1,34 @@
+# This week’s exercise is going to be revisiting an old exercise (see Exercise 5), except require the solution in a different way.
+
+# Take two lists, say for example these two:
+
+#     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#     b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+# and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes. Write this in one line of Python using at least one list comprehension. (Hint: Remember list comprehensions from Exercise 7).
+
+# The original formulation of this exercise said to write the solution using one line of Python, but a few readers pointed out that this was impossible to do without using sets that I had not yet discussed on the blog, so you can either choose to use the original directive and read about the set command in Python 3.3, or try to implement this on your own and use at least one list comprehension in the solution.
+
+# Extra:
+
+#     Randomly generate two lists to test this
+
+import random
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+
+# generate list of random numbers from 0-100
+random_a_len = random.randint(0,25)
+random_a = [random.randint(0, 50) for x in range(random_a_len)]
+
+c = [num for num in set(a) if num in b]
+print(c)
+
+# generate list of random numbers from 0-100
+random_b_len = random.randint(0,25)
+random_b = [random.randint(0, 50) for x in range(random_b_len)]
+
+random_c = [num for num in set(random_a) if num in random_b]
+print(random_c)
